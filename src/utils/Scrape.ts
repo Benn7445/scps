2dcabedff01046d458f87d8c77fb3b1bb0ea00cd
DevQuartz <cookiebenno@gmail.com>
2022-07-21 00:01:50 +0200

    New Rewrite

--- src/utils/Scrape.ts
+++ src/utils/Scrape.ts
@@ -1,5 +1,24 @@
 export const scrapleLink = async (link: string) => {
-  const response = await fetch("/sc/" + link);
+  const response = await fetch("/sc/" + link, {
+    headers: {
+      accept: "*/*",
+      "accept-language": "en-US,en;q=0.9,nl-NL;q=0.8,nl;q=0.7",
+      "cache-control": "max-age=0",
+      "sec-ch-ua":
+        '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": '"Windows"',
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-site",
+    },
+    referrer: "https://www.sofascore.com/",
+    referrerPolicy: "strict-origin-when-cross-origin",
+    body: null,
+    method: "GET",
+    mode: "cors",
+    credentials: "omit",
+  });
   const body = await response.json();
   let starting = 0;
   for (let i = body.events.length - 1; i > 0; i--) {
